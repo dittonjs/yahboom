bbad5ec179e014037c1a420d0fefcaed33949c16
Joseph Ditton <jditton.atomic@gmail.com>
2021-11-11 13:15:39 -0700

    adds start

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# remove old src zip
+rm src.tar.gz
+
+# zip up src directory
+tar -zcvf src.tar.gz src
+
+# remove zip remote
+echo "Cleaning up existing deploy"
+ssh pi@192.168.50.1 "cd ~/python; rm src.tar.gz; rm -r src"
+
+echo "Deploying"
+scp src.tar.gz pi@192.168.50.1:~/python/src.tar.gz
+
+echo "Installing"
+ssh pi@192.168.50.1 "cd ~/python; tar -xvzf src.tar.gz"
+
+echo "Deploy complete! run 'ssh pi@192.168.50.1' to remote into robot"
+
---
+++ src/yahboom_tank.py
@@ -0,0 +1,5 @@
+#-*- coding:UTF-8 -*-
+
+class YahboomTank:
+  def __init__(self):
+    self.left_
